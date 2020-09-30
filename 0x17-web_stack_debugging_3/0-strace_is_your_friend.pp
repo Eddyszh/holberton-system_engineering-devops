e4653547e0e2344642feacb1e4789dde161f8fc4
eddyszh <eddyszh@gmail.com>
2020-09-30 23:35:21 +0000

    Learn about debugging 3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 #Fix the path of the php
-exect { 'fix_that_path':
+exec { 'fix_that_path':
     command => 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php',
     path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
     onlyif  => 'test -f /var/www/html/wp-settings.php'
